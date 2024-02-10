6a8e89d028ba5ac2c1d061fd1f5b413ba8edd94f
pakdipong <157682440+pakdipong@users.noreply.github.com>
2024-02-10 16:45:43 +0700

    sign-in-with-google

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var passport = require('passport');
+var SQLiteStore = require('connect-sqlite3')(session);
+var indexRouter = require('./routes/index');
+var authRouter = require('./routes/auth');
 
 var indexRouter = require('./routes/index');
 
@@ -22,15 +27,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
+}));
+
+app.use(passport.authenticate('session'));
+
 app.use('/', indexRouter);
+app.use('/', authRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
